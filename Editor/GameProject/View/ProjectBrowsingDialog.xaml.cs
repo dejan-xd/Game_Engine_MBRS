1cdb9215d26633c1acc04774dca62aed04750ca5
dejan-xd <dejan.jovanovic94@gmail.com>
2022-03-27 15:27:20 +0200

    Create project templates and new game project. Close #3 and Close #4

---
+++ Editor/GameProject/View/ProjectBrowsingDialog.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+
+namespace Editor.GameProject
+{
+    /// <summary>
+    /// Interaction logic for ProjectBrowsingDialog.xaml
+    /// </summary>
+    public partial class ProjectBrowsingDialog : Window
+    {
+        public ProjectBrowsingDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenProjBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender == openProjBtn)
+            {
+                if (createProjBtn.IsChecked == true)
+                {
+                    createProjBtn.IsChecked = false;
+                    browserStackPanel.Margin = new Thickness(0);
+                }
+                openProjBtn.IsChecked = true;
+            }
+            else
+            {
+                if (openProjBtn.IsChecked == true)
+                {
+                    openProjBtn.IsChecked = false;
+                    browserStackPanel.Margin = new Thickness(-800, 0, 0, 0);
+                }
+                createProjBtn.IsChecked = true;
+            }
+        }
+    }
+}
