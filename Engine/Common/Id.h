abe78633da2420ea4357b3c2288110b9685932cc
dejan-xd <dejan.jovanovic94@gmail.com>
2022-04-04 21:02:49 +0200

    Game Engine entity and component implementation. Close #15

--- Engine/Common/Id.h
+++ Engine/Common/Id.h
@@ -4,32 +4,60 @@
 namespace primal::id {
 	using id_type = u32;
 
-	constexpr u32 generation_bits{ 8 };
-	constexpr u32 index_bits{ sizeof(id_type) * 8 - generation_bits };
-	constexpr id_type index_mask{ id_type(1) << index_bits - 1 };
-	constexpr id_type generation_mask{ id_type(1) << generation_bits - 1 };
-	constexpr id_type id_mask{ id_type{-1} };
+	// internal namespace (not supposed to use these)
+	namespace internal {
+		constexpr u32 generation_bits{ 8 };
+		constexpr u32 index_bits{ sizeof(id_type) * 8 - generation_bits };
+		constexpr id_type index_mask{ (id_type(1) << index_bits) - 1 };
+		constexpr id_type generation_mask{ (id_type(1) << generation_bits) - 1 };
+	}
 
-	using generation_type = std::conditional_t<generation_bits <= 16, std::conditional_t<generation_bits <= 8, u8, u16>, u32>;
+	constexpr id_type invalid_id{ id_type(-1) };
+	constexpr u32 min_deleted_elements{ 1024 };
 
-	static_assert(sizeof(generation_type) * 8 >= generation_bits);
+	using generation_type = std::conditional_t<internal::generation_bits <= 16, std::conditional_t<internal::generation_bits <= 8, u8, u16>, u32>;
+	static_assert(sizeof(generation_type) * 8 >= internal::generation_bits);
 	static_assert((sizeof(id_type) - sizeof(generation_type)) > 0);
 
-	inline bool is_valid(id_type id) {
-		return id != id_mask;
+	constexpr bool is_valid(id_type id) {
+		return id != invalid_id;
 	}
 
-	inline id_type index(id_type id) {
-		return id & index_mask;
+	constexpr id_type index(id_type id) {
+		id_type index{ id & internal::index_mask };
+		assert(index != internal::index_mask);
+		return index;
 	}
 
-	inline id_type generation(id_type id) {
-		return (id >> index_bits) & generation_mask;
+	constexpr id_type generation(id_type id) {
+		return (id >> internal::index_bits) & internal::generation_mask;
 	}
 
-	inline id_type new_generation(id_type id) {
+	constexpr id_type new_generation(id_type id) {
 		const id_type generation{ id::generation(id) + 1 };
-		assert(generation < 255);
-		return index(id) | (generation << index_bits);
+		assert(generation < (((u64)1 << internal::generation_bits) - 1));
+		return index(id) | (generation << internal::index_bits);
 	}
+
+#if _DEBUG
+	// internal namespace
+	namespace internal {
+		struct id_base {
+			constexpr explicit id_base(id_type id) : _id{ id } {}
+			constexpr operator id_type() const { return _id; }
+
+		private:
+			id_type _id;
+		};
+	}
+
+#define DEFINE_TYPED_ID(name)														\
+		struct name final : id::internal::id_base {									\
+			constexpr explicit name(id::id_type id) : id_base{ id } {}				\
+			constexpr name() : id_base{ 0 } {}										\
+		};
+
+#else
+#define DEFINE_TYPED_ID(name) using name = id::id_type;
+#endif
 }
