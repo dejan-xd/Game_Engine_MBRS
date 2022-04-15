9ab21cff345f7c1126c1d2274b6821b55535fb28
dejan-xd <dejan.jovanovic94@gmail.com>
2022-04-15 21:13:23 +0200

    EngineDLL API bridge. Close #17

--- Editor/GameProject/ViewModel/Scene.cs
+++ Editor/GameProject/ViewModel/Scene.cs
@@ -5,55 +5,12 @@ using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Runtime.Serialization;
 using System.Windows.Input;
-using Editor.GameProject.ViewModel;
-using Editor.WrappersDLL;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace Editor.GameProject.ViewModel
 {
     [DataContract(Namespace = "http://schemas.datacontract.org/2004/07/Editor.GameProject")]
     internal class Scene : ViewModelBase
     {
-        private int _entityId = ID.INVALID_ID;
-        public int EntityId
-        {
-            get => _entityId;
-            set
-            {
-                if (_entityId != value)
-                {
-                    _entityId = value;
-                    OnPropertyChanged(nameof(EntityId));
-                }
-            }
-        }
-
-        private bool _isActive;
-        public bool IsActive
-        {
-            get => _isActive;
-            set
-            {
-                if (_isActive != value)
-                {
-                    _isActive = value;
-                    if (_isActive)
-                    {
-                        EntityId = EngineAPI.CreateGameEntity(this);
-                        Debug.Assert(ID.IsValid(_entityId));
-                    }
-                    else
-                    {
-                        EngineAPI.RemoveGameEntity(this);
-                    }
-
-                    OnPropertyChanged(nameof(IsActive));
-                }
-            }
-        }
-
         private string _name;
         [DataMember]
         public string Name
@@ -72,6 +29,21 @@ namespace Editor.GameProject.ViewModel
         [DataMember]
         public Project Project { get; private set; }
 
+        private bool _isActive;
+        [DataMember]
+        public bool IsActive
+        {
+            get => _isActive;
+            set
+            {
+                if (_isActive != value)
+                {
+                    _isActive = value;
+                    OnPropertyChanged(nameof(IsActive));
+                }
+            }
+        }
+
         [DataMember(Name = nameof(GameEntities))]
         private readonly ObservableCollection<GameEntity> _gameEntities = new();
         public ReadOnlyObservableCollection<GameEntity> GameEntities { get; private set; }
@@ -79,15 +51,24 @@ namespace Editor.GameProject.ViewModel
         public ICommand AddGameEntityCommand { get; private set; }
         public ICommand RemoveGameEntityCommand { get; private set; }
 
-        private void AddGameEntity(GameEntity entity)
+        private void AddGameEntity(GameEntity entity, int index = -1)
         {
             Debug.Assert(!_gameEntities.Contains(entity));
-            _gameEntities.Add(entity);
+            entity.IsActive = IsActive; // in case inactive scene could be able to modify entities (flexible)
+            if (index == -1)
+            {
+                _gameEntities.Add(entity);
+            }
+            else
+            {
+                _gameEntities.Insert(index, entity);
+            }
         }
 
         private void RemoveGameEntity(GameEntity entity)
         {
             Debug.Assert(_gameEntities.Contains(entity));
+            entity.IsActive = false;
             _gameEntities.Remove(entity);
         }
 
@@ -100,6 +81,11 @@ namespace Editor.GameProject.ViewModel
                 OnPropertyChanged(nameof(GameEntities));
             }
 
+            foreach (GameEntity entity in _gameEntities)
+            {
+                entity.IsActive = IsActive;
+            }
+
             AddGameEntityCommand = new RelayCommand<GameEntity>(x =>
             {
                 AddGameEntity(x);
@@ -107,7 +93,7 @@ namespace Editor.GameProject.ViewModel
 
                 Project.UndoRedo.Add(new UndoRedoAction(
                     () => RemoveGameEntity(x),
-                    () => _gameEntities.Insert(index, x),
+                    () => AddGameEntity(x, index),
                     $"Add {x.Name} to {Name}"));
             });
 
@@ -117,7 +103,7 @@ namespace Editor.GameProject.ViewModel
                 RemoveGameEntity(x);
 
                 Project.UndoRedo.Add(new UndoRedoAction(
-                    () => _gameEntities.Insert(index, x),
+                    () => AddGameEntity(x, index),
                     () => RemoveGameEntity(x),
                     $"Remove {x.Name}"));
             });
--- Engine/Utilities/MathTypes.h
+++ Engine/Utilities/MathTypes.h
@@ -2,7 +2,7 @@
 #include "CommonHeaders.h"
 
 namespace primal::math {
-	constexpr float pi = 3.1415926535897932384626433832795028841971;
+	constexpr float pi = 3.1415926535897932384626433832795028841971f;
 	constexpr float epsilon = 1e-5f;
 
 #if defined(_WIN64)
