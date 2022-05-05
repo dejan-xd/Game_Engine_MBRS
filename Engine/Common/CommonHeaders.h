a4ea5c1f008e72f57f137d90a1e2d64020fae676
Jovan <jovanpop92@gmail.com>
2022-05-06 00:24:53 +0200

    Open game window Close #50

--- Engine/Common/CommonHeaders.h
+++ Engine/Common/CommonHeaders.h
@@ -16,3 +16,4 @@
 #include "..\Utilities\Utilities.h"
 #include "..\Utilities\MathTypes.h"
 #include "PrimitiveTypes.h"
+#include "Id.h"
---
+++ Engine/Platform/Platform.cpp
@@ -0,0 +1,270 @@
+#include "Platform.h"
+#include "PlatformTypes.h"
+
+namespace primal::platform {
+
+#ifdef _WIN64
+	namespace {
+		struct window_info {
+			HWND hwnd{ nullptr };
+			RECT client_area{ 0, 0, 1920, 1080 };
+			RECT fullscreen_area{};
+			POINT top_left{ 0, 0 };
+			DWORD style{ WS_VISIBLE };
+			bool is_fullscreen{ false };
+			bool is_closed{ false };
+		};
+
+		utl::vector<window_info> windows;
+		////////////////////////////////////////////////////////////////////
+		// TODO: this part will be handled by a free-list container later on
+		utl::vector<u32> available_slots;
+
+		u32 add_to_windows(window_info info) {
+			u32 id{ u32_invalid_id };
+			if (available_slots.empty()) {
+				id = (u32)windows.size();
+				windows.emplace_back(info);
+			}
+			else {
+				id = available_slots.back();
+				available_slots.pop_back();
+				assert(id != u32_invalid_id);
+				windows[id] = info;
+			}
+			return id;
+		}
+
+		void remove_from_windows(u32 id) {
+			assert(id < windows.size());
+			available_slots.emplace_back(id);
+		}
+		////////////////////////////////////////////////////////////////////
+
+		window_info& get_from_id(window_id id) {
+			assert(id < windows.size());
+			assert(windows[id].hwnd);
+			return windows[id];
+		}
+
+		window_info& get_from_handle(window_handle handle) {
+			const window_id id{ (id::id_type)GetWindowLongPtr(handle, GWLP_USERDATA) };
+			return get_from_id(id);
+		}
+
+		LRESULT CALLBACK internal_window_proc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+			// update the window
+			window_info* info{ nullptr };
+
+			// TODO: write a test program for creating a windows and see what messages will be recived
+			//		 and implement switch cases for those we need to handle
+			switch (msg) {
+			case WM_DESTROY:
+				get_from_handle(hwnd).is_closed = true;
+				break;
+			}
+
+			LONG_PTR long_ptr{ GetWindowLongPtr(hwnd, 0) };
+			return long_ptr ? ((window_proc)long_ptr)(hwnd, msg, wparam, lparam) : DefWindowProc(hwnd, msg, wparam, lparam);
+		}
+
+		void resize_window(const window_info& info, const RECT& area) {
+			// adjust the window size for correct device size
+			RECT window_rect{ area };
+			AdjustWindowRect(&window_rect, info.style, FALSE);
+
+			const s32 width{ window_rect.right - window_rect.left };
+			const s32 height{ window_rect.bottom - window_rect.top };
+
+			MoveWindow(info.hwnd, info.top_left.x, info.top_left.y, width, height, true);
+		}
+
+		void resize_window(window_id id, u32 width, u32 height) {
+			window_info& info{ get_from_id(id) };
+			// NOTE: resize while in fullscreen mode to support the case when the user changes the screen resoultion
+			RECT& area{ info.is_fullscreen ? info.fullscreen_area : info.client_area };
+			area.right = area.left + width;
+			area.bottom = area.top + height;
+
+			resize_window(info, area);
+		}
+
+		void set_window_fullscreen(window_id id, bool is_fullscreen) {
+			window_info& info{ get_from_id(id) };
+			if (info.is_fullscreen != is_fullscreen) {
+				info.is_fullscreen = is_fullscreen;
+
+				if (is_fullscreen) {
+					// store the current window dimensions so they can be restored
+					// when switching out of fullscreen state
+					GetClientRect(info.hwnd, &info.client_area);
+					RECT rect;
+					GetWindowRect(info.hwnd, &rect);
+					info.top_left.x = rect.left;
+					info.top_left.y = rect.top;
+					info.style = 0;
+					SetWindowLongPtr(info.hwnd, GWL_STYLE, info.style);
+					ShowWindow(info.hwnd, SW_MAXIMIZE);
+				}
+				else {
+					info.style = WS_VISIBLE | WS_OVERLAPPEDWINDOW;
+					SetWindowLongPtr(info.hwnd, GWL_STYLE, info.style);
+					resize_window(info, info.client_area);
+					ShowWindow(info.hwnd, SW_SHOWNORMAL);
+				}
+			}
+		}
+
+		bool is_window_fullscreen(window_id id) {
+			return get_from_id(id).is_fullscreen;
+		}
+
+		window_handle get_window_handle(window_id id) {
+			return get_from_id(id).hwnd;
+		}
+
+		void set_window_caption(window_id id, const wchar_t* caption) {
+			window_info& info{ get_from_id(id) };
+			SetWindowText(info.hwnd, caption);
+		}
+
+		math::u32v4 get_window_size(window_id id) {
+			window_info& info{ get_from_id(id) };
+			RECT area{ info.is_fullscreen ? info.fullscreen_area : info.client_area };
+			return { (u32)area.left, (u32)area.top, (u32)area.right, (u32)area.bottom };
+		}
+
+		bool is_window_closed(window_id id) {
+			return get_from_id(id).is_closed;
+		}
+	} // anonymous namespace
+
+	window create_window(const window_init_info* const init_info /* = nullptr */) {
+		window_proc callback{ init_info ? init_info->callback : nullptr };
+		window_handle parent{ init_info ? init_info->parent : nullptr };
+
+		// 1. setup a window class
+		WNDCLASSEX wc; // alt + F12, ctrl + click on WNDCLASSEXW and copy - paste its (tagWNDCLASSEXW) struct
+		ZeroMemory(&wc, sizeof(wc));
+		wc.cbSize = sizeof(WNDCLASSEX);
+		/* Win 3.x */
+		wc.style = CS_HREDRAW | CS_VREDRAW;
+		wc.lpfnWndProc = internal_window_proc;
+		wc.cbClsExtra = 0;
+		wc.cbWndExtra = callback ? sizeof(callback) : 0;
+		wc.hInstance = 0;
+		wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+		wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+		wc.hbrBackground = CreateSolidBrush(RGB(168, 204, 215)); // 152, 158, 161; default - 26, 48, 76
+		wc.lpszMenuName = NULL;
+		wc.lpszClassName = L"GameEngine Window";
+		/* Win 4.0 */
+		wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+		// 2. register the window class
+		RegisterClassEx(&wc);
+
+		// instance of window information
+		window_info info{};
+		RECT rc{ info.client_area };
+
+		// adjust the client area (the content window without tittle bar and the edges) for correct device size
+		AdjustWindowRect(&rc, info.style, FALSE);
+
+		// checking if values exists, if not take default value
+		const wchar_t* caption{ (init_info && init_info->caption) ? init_info->caption : L"GameEngine" };
+		const s32 left{ (init_info && init_info->left) ? init_info->left : info.client_area.left };
+		const s32 top{ (init_info && init_info->top) ? init_info->top : info.client_area.top };
+		const s32 width{ (init_info && init_info->width) ? init_info->width : rc.right - rc.left };
+		const s32 height{ (init_info && init_info->height) ? init_info->height : rc.bottom - rc.top };
+
+		// define style for the window
+		info.style |= parent ? WS_CHILD : WS_OVERLAPPEDWINDOW;
+
+		// 3. create an instance of window class
+		info.hwnd = CreateWindowEx(
+			0,					// extended style
+			wc.lpszClassName,	// window class name
+			caption,			// instance title
+			info.style,			// window style
+			left, top,			// initial window position
+			width, height,		// initial window dimensions
+			parent,				// handle to parent window
+			NULL,				// handle to menu
+			NULL,				// instance of this application
+			NULL				// extra creation parameters
+		);
+
+		if (info.hwnd) {
+			const window_id id{ add_to_windows(info) };
+			SetWindowLongPtr(info.hwnd, GWLP_USERDATA, (LONG_PTR)id);
+
+			// set in the "extra" bytes the pointer to the window callback function
+			// which handles messages for the window
+			if (callback) SetWindowLongPtr(info.hwnd, 0, (LONG_PTR)callback);
+			assert(GetLastError() == 0);
+			ShowWindow(info.hwnd, SW_SHOWNORMAL);
+			UpdateWindow(info.hwnd);
+
+			return window{ id };
+		}
+		return{};
+	}
+
+	void remove_window(window_id id) {
+		window_info& info{ get_from_id(id) };
+		DestroyWindow(info.hwnd);
+		remove_from_windows(id);
+	}
+
+#elif
+#error "must implement at least one platform"
+#endif // _WIN64
+
+	// PLATFORM INDEPENDENT METHODS
+
+	void window::set_fullscreen(bool is_fullscreen) const {
+		assert(is_valid());
+		set_window_fullscreen(_id, is_fullscreen);
+	}
+
+	bool window::is_fullscreen() const {
+		assert(is_valid());
+		return is_window_fullscreen(_id);
+	}
+
+	void* window::handle() const {
+		assert(is_valid());
+		return get_window_handle(_id);
+	}
+
+	void window::set_caption(const wchar_t* caption) const {
+		assert(is_valid());
+		set_window_caption(_id, caption);
+	}
+
+	const math::u32v4 window::size() const {
+		assert(is_valid());
+		return get_window_size(_id);
+	}
+
+	void window::resize(u32 width, u32 height) const {
+		assert(is_valid());
+		resize_window(_id, width, height);
+	}
+
+	const u32 window::width() const {
+		math::u32v4 s{ size() };
+		return s.z - s.x;
+	}
+
+	const u32 window::height() const {
+		math::u32v4 s{ size() };
+		return s.w - s.y;
+	}
+
+	bool window::is_closed() const {
+		assert(is_valid());
+		return is_window_closed(_id);
+	}
+}
