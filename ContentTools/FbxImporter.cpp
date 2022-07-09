722b650213d9a038d2eafdf7ea5b1ab4b90de247
nole23 <nole0223@gmail.com>
2022-07-09 20:13:23 +0200

    Get vertex data and LODs from FBX files. Close #99

--- ContentTools/FbxImporter.cpp
+++ ContentTools/FbxImporter.cpp
@@ -1,11 +1,13 @@
 #include "FbxImporter.h"
 #include "Geometry.h"
-#include <mutex>
 
 // If you get any compilation or linker errors than make sure that:
 // 1) FBS SDK 2020.2 or later version is installed on your system
 // 2) the include path to fbxsdk.h is added to "Additional Include Directories" (compiler settings)
 // 3) the library paths in the following secion point to the correct location
+
+// LNK4099 PDB not found warnings can be resolved either by installing FBX SDK PDBs (separate download) or
+// by disabling this warning in linker options (Linker command line: /ignore:4099)
 
 #if _DEBUG
 #pragma comment(lib, "C:\\Program Files\\Autodesk\\FBX\\FBX SDK\\2020.2\\lib\\vs2019\\x64\\debug\\libfbxsdk-md.lib")
@@ -77,7 +79,8 @@ namespace primal::tools {
 				get_lod_group(node);
 			}
 			else {
-				get_scene(node); // see if there is a mesh somewhere further down the hierarchy
+				// see if there is a mesh somewhere further down the hierarchy
+				get_scene(node);
 			}
 
 		}
@@ -99,15 +102,148 @@ namespace primal::tools {
 			m.lod_threshold = -1.f;
 			m.name = (node->GetName()[0] != '\0') ? node->GetName() : fbx_mesh->GetName();
 
-			//if (get_mesh_data(fbx_mesh, m)) {
-			//	meshes.emplace_back(m);
-			//}
+			if (get_mesh_data(fbx_mesh, m)) {
+				meshes.emplace_back(m);
+			}
 		}
+
+		// see if there is a mesh somewhere further down the hierarchy
+		get_scene(node);
 	}
 
 	void fbx_context::get_lod_group(FbxNode* node) {
+		assert(node);
+		if (FbxLODGroup * lod_grp{ node->GetLodGroup() }) {
+			lod_group lod{};
+			lod.name = (node->GetName()[0] != '\0') ? node->GetName() : lod_grp->GetName();
+			// NOTE: number of LODs is exclusive the base mesh (LOD 0)
+			const s32 num_lods{ lod_grp->GetNumThresholds() };
+			const s32 num_nodes{ node->GetChildCount() };
+			assert(num_lods > 0 && num_nodes > 0);
 
+			for (s32 i{ 0 }; i < num_nodes; ++i) {
+				get_mesh(node->GetChild(i), lod.meshes);
+
+				if (lod.meshes.size() > 1 && lod.meshes.size() <= num_lods + 1 && lod.meshes.back().lod_threshold < 0.f) {
+					FbxDistance threshold;
+					lod_grp->GetThreshold((u32)lod.meshes.size() - 2, threshold);
+					lod.meshes.back().lod_threshold = threshold.value() * _scene_scale;
+				}
+			}
+
+			if (lod.meshes.size()) _scene->lod_groups.emplace_back(lod);
+		}
 	}
+
+	bool fbx_context::get_mesh_data(FbxMesh* fbx_mesh, mesh& m) {
+		assert(fbx_mesh);
+
+		const s32 num_polys{ fbx_mesh->GetPolygonCount() };
+		if (num_polys <= 0) return false;
+
+		// get vertices
+		const s32 num_vertices{ fbx_mesh->GetControlPointsCount() };
+		FbxVector4* vertices{ fbx_mesh->GetControlPoints() };
+		const s32 num_indices{ fbx_mesh->GetPolygonVertexCount() };
+		s32* indices{ fbx_mesh->GetPolygonVertices() };
+
+		assert(num_vertices > 0 && vertices && num_indices > 0 && indices);
+		if (!(num_vertices > 0 && vertices && num_indices > 0 && indices)) return false;
+
+		m.raw_indices.resize(num_indices);
+		utl::vector vertex_ref(num_vertices, u32_invalid_id);
+
+		for (s32 i{ 0 }; i < num_indices; ++i) {
+			const u32 v_idx{ (u32)indices[i] };
+			// did we encounter this vertex before? 
+			// if so, just add its index, if not, add the vertex and a new index
+			if (vertex_ref[v_idx] != u32_invalid_id) {
+				m.raw_indices[i] = vertex_ref[v_idx];
+			}
+			else {
+				FbxVector4 v = vertices[v_idx] * _scene_scale;
+				m.raw_indices[i] = (u32)m.positions.size();
+				vertex_ref[v_idx] = m.raw_indices[i];
+				m.positions.emplace_back((f32)v[0], (f32)v[1], (f32)v[2]);
+			}
+		}
+
+		assert(m.raw_indices.size() % 3 == 0);
+
+		// get material index per polygon
+		assert(num_polys > 0);
+		FbxLayerElementArrayTemplate<s32>* mtl_indices;
+		if (fbx_mesh->GetMaterialIndices(&mtl_indices)) {
+			for (s32 i{ 0 }; i < num_polys; ++i) {
+				const s32 mtl_index{ mtl_indices->GetAt(i) };
+				assert(mtl_index >= 0);
+				m.material_indices.emplace_back((u32)mtl_index);
+				if (std::find(m.material_used.begin(), m.material_used.end(), (u32)mtl_index) == m.material_used.end()) {
+					m.material_used.emplace_back((u32)mtl_index);
+				}
+			}
+		}
+
+		// importing normals is ON by default
+		const bool import_normals{ !_scene_data->settings.calculate_normals };
+		// importing tangents is OFF by default
+		const bool import_tangents{ !_scene_data->settings.calculate_tangents };
+
+		// import normals
+		if (import_normals) {
+			FbxArray<FbxVector4> normals;
+			// calculate normals using FBX's built-in method, but only if no normal data is already there
+			if (fbx_mesh->GenerateNormals() && fbx_mesh->GetPolygonVertexNormals(normals) && normals.Size() > 0) {
+				const s32 num_normals{ normals.Size() };
+				for (s32 i{ 0 }; i < num_normals; ++i) {
+					m.normals.emplace_back((f32)normals[i][0], (f32)normals[i][1], (f32)normals[i][2]);
+				}
+			}
+			else {
+				// something went wrong with importing normals from FBX
+				// fall back to our own normal calculation method
+				_scene_data->settings.calculate_normals = true;
+			}
+		}
+
+		// import triangles
+		if (import_tangents) {
+			FbxLayerElementArrayTemplate<FbxVector4>* tangents{ nullptr };
+			// calculate tangents using FBX's built-in method, but only if no tangent data is already there
+			if (fbx_mesh->GenerateTangentsData() && fbx_mesh->GetTangents(&tangents) && tangents && tangents->GetCount() > 0) {
+				const s32 num_tangent{ tangents->GetCount() };
+				for (s32 i{ 0 }; i < num_tangent; ++i) {
+					FbxVector4 t{ tangents->GetAt(i) };
+					m.tangents.emplace_back((f32)t[0], (f32)t[1], (f32)t[2], (f32)t[3]);
+				}
+			}
+			else {
+				// something went wrong with importing tangents from FBX
+				// fall back to our own tangent calculation method
+				_scene_data->settings.calculate_tangents = true;
+			}
+		}
+
+		// get UVs
+		FbxStringList uv_names;
+		fbx_mesh->GetUVSetNames(uv_names);
+		const s32 uv_set_count{ uv_names.GetCount() };
+		// NOTE: it's ok if we don't have an UV set. For example, some emissive objects don't need an UV map
+		m.uv_sets.resize(uv_set_count);
+
+		for (s32 i{ 0 }; i < uv_set_count; ++i) {
+			FbxArray<FbxVector2> uvs;
+			if (fbx_mesh->GetPolygonVertexUVs(uv_names.GetStringAt(i), uvs)) {
+				const s32 num_uvs{ uvs.Size() };
+				for (s32 j{ 0 }; j < num_uvs; ++j) {
+					m.uv_sets[i].emplace_back((f32)uvs[j][0], (f32)uvs[j][1]);
+				}
+			}
+		}
+
+		return true;
+	}
+
 
 	EDITOR_INTERFACE void ImportFbx(const char* file, scene_data* data) {
 		assert(file && data);
@@ -118,7 +254,7 @@ namespace primal::tools {
 			std::lock_guard lock{ fbx_mutex };
 			fbx_context fbx_context{ file, &scene, data };
 			if (fbx_context.is_valid()) {
-				// get scene
+				fbx_context.get_scene();
 			}
 			else {
 				// TODO: send failure log message to editor
@@ -129,4 +265,4 @@ namespace primal::tools {
 		process_scene(scene, data->settings);
 		pack_data(scene, *data);
 	}
-}
+} // namespace primal::tools
