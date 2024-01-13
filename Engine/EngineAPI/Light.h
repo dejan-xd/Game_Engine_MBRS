46ed11dbd44edaf318cc962685178f3ee4bb2ddc
Dejan Jovanović <dejan.jovanovic94@gmail.com>
2024-01-13 18:58:05 +0100

    Tightly packed light arrays. Close #180

--- Engine/EngineAPI/Light.h
+++ Engine/EngineAPI/Light.h
@@ -17,7 +17,7 @@ namespace primal::graphics {
 			count
 		};
 
-		constexpr explicit light(light_id id, u64 light_set_key) : _light_set_key{ light_set_key }, _id { id } {}
+		constexpr explicit light(light_id id, u64 light_set_key) : _light_set_key{ light_set_key }, _id{ id } {}
 		constexpr light() = default;
 		constexpr light_id get_id() const { return _id; }
 		constexpr u64 light_set_key() const { return _light_set_key; }
@@ -26,10 +26,17 @@ namespace primal::graphics {
 		void is_enabled(bool is_enabled) const;
 		void intensity(f32 intensity) const;
 		void color(math::v3 color) const;
+		void attenuation(math::v3 attenuation) const;
+		void range(f32 range) const;
+		void cone_angles(f32 umbra, f32 penumbra) const;
 
 		bool is_enabled() const;
 		f32 intensity() const;
 		math::v3 color() const;
+		math::v3 attenuation() const;
+		f32 range() const;
+		f32 umbra() const;
+		f32 penumbra() const;
 		type light_type() const;
 		id::id_type entity_id() const;
 
